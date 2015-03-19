07384b62cfc018e6e79d914dce09fc0741952b82
Stanislav Nikitin <sv.nikitin@physics.msu.ru>
2015-03-19 20:43:01 +0300

    Bash script to install Ubuntu packages added

---
+++ my_inst_pack.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+sudo apt-get install aptitude
+sudo aptitude install aircrack-ng ap-hotspot chromium-browser djview dos2unix driftnet elinks g++ gedit-plugins ghostscript gimp git gnash  gnuplot gnuplot-doc gnuplot-x11  imagemagick inxi ipython macchanger mc mercurial midori mpich nautilus-open-terminal pinta putty putty-tools q4wine qtcreator qtcreator-doc reaver scidavis skype spyder texlive texlive-lang-cyrillic texstudio traceroute  transmission-common transmission-gtk vim virtualbox vlc wine 
