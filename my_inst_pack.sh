98a71e0f09518114be39b33c5616764fcc61e156
stan <citrus_82@nextmail.ru>
2015-03-31 22:47:43 +0300

    Upd. Regular.

--- my_inst_pack.sh
+++ my_inst_pack.sh
@@ -1,3 +1,10 @@
 #/bin/bash
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:tualatrix/ppa
+
+sudo apt-get update
+
+
 sudo apt-get install aptitude
-sudo aptitude install aircrack-ng ap-hotspot chromium-browser cmake djview dos2unix driftnet elinks g++ gedit-plugins ghostscript gimp git gksu gnash  gnuplot gnuplot-doc gnuplot-x11  gparted htop imagemagick inxi ipython kmines laptop-mode-tools macchanger mc mercurial midori mpich nautilus-open-terminal pdf2djvu pinta putty putty-tools q4wine qtcreator qtcreator-doc rar reaver scidavis skype spyder texlive texlive-lang-cyrillic texstudio traceroute  transmission-common transmission-gtk vim virtualbox vkaudiosaver vlc wine 
+
+sudo aptitude install aircrack-ng ambiance-dark ap-hotspot chromium-browser cmake deluge djview dos2unix driftnet elinks g++ gedit-plugins ghostscript gimp git gksu gnash  gnuplot gnuplot-doc gnuplot-x11  gparted htop imagemagick inxi ipython kmines laptop-mode-tools macchanger mc mercurial midori mpich nautilus-open-terminal pdf2djvu pinta putty putty-tools q4wine qtcreator qtcreator-doc rar reaver scidavis skype spyder texlive texlive-lang-cyrillic texstudio traceroute ubuntu-tweak vim virtualbox vkaudiosaver vlc wine 
